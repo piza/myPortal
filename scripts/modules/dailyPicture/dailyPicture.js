ffbe04cd7a429d7ab715ed69b067cf431e35473a
Peter <wenbin.cao@ktbhealth.com>
2016-05-11 19:26:24 +0800

    save pic to qiniu

--- scripts/modules/dailyPicture/dailyPicture.js
+++ scripts/modules/dailyPicture/dailyPicture.js
@@ -4,6 +4,11 @@ define(['backbone','template','layer','dataCenter'], function(Backbone,temp,laye
         url:"http://api.01cun.com/dailyPicture"
     });
 
+    var QiniuModel=dc.BaseModel.extend({
+        url:'http://api.01cun.com/qiniu/dailyPicture',
+        idAttribute:"translate"
+    });
+
     var PictureList=dc.PaginatedCollection.extend({
         model:Picture
     });
@@ -11,6 +16,9 @@ define(['backbone','template','layer','dataCenter'], function(Backbone,temp,laye
     var pList=new PictureList({baseUrl:"http://api.01cun.com/dailyPicture/list"});
     var DailyPicture=dc.BaseView.extend({
 
+        events:{
+            "click .savePic":"savePic"
+        },
         initialize:function(){
            this.pagingView=new  dc.PaginatedView({collection:pList});
             pList.bind("reset",this.renderList,this);
@@ -31,6 +39,15 @@ define(['backbone','template','layer','dataCenter'], function(Backbone,temp,laye
             var that=this;
             pList.each(function(picture){
                 that.$("#pictureList").append(temp.dailyPicture.pictureItem({item:picture.toJSON()}))
+            });
+
+        },
+        savePic:function(e){
+            var id=$(e.target).attr('data-id');
+            var that=this;
+            var qiniuModel=new QiniuModel();
+            qiniuModel.save({"id":id},function(res){
+                layer.msg("收藏成功,后台正在处理!");
             });
         }
     });
