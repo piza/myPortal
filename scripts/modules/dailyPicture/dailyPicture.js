61e3cad8915e7e0183884597938fd0c82673f394
Peter <wenbin.cao@ktbhealth.com>
2016-05-12 15:39:46 +0800

    add thumbnail

--- scripts/modules/dailyPicture/dailyPicture.js
+++ scripts/modules/dailyPicture/dailyPicture.js
@@ -1,7 +1,21 @@
-define(['backbone','template','layer','dataCenter'], function(Backbone,temp,layer,dc){
+define(['backbone','template','layer','layerExt','dataCenter'], function(Backbone,temp,layer,layerExt,dc){
 
     var Picture=dc.BaseModel.extend({
-        url:"http://api.01cun.com/dailyPicture"
+        url:"http://api.01cun.com/dailyPicture",
+        getImgSrc:function(){
+        var imgUrl=this.get("originUrl");
+        if(this.get("savedUrl")){
+            imgUrl= "//qiniu.01cun.com/"+this.get("savedUrl");
+        }
+        return imgUrl;
+    },
+    getImgThumbnailSrc:function(width){
+        var imgUrl=this.get("originUrl");
+        if(this.get("savedUrl")){
+            imgUrl= "//qiniu.01cun.com/"+this.get("savedUrl")+"?imageView2/1/w/"+width+"/h/"+width;
+        }
+        return imgUrl;
+    }
     });
 
     var QiniuModel=dc.BaseModel.extend({
@@ -14,11 +28,43 @@ define(['backbone','template','layer','dataCenter'], function(Backbone,temp,laye
     });
 
     var pList=new PictureList({baseUrl:"http://api.01cun.com/dailyPicture/list"});
-    var DailyPicture=dc.BaseView.extend({
-
+    var thumbnailData=new Backbone.Collection();
+    var ThumbnailView=Backbone.View.extend({
+        className:"col-md-4",
         events:{
-            "click .savePic":"savePic"
+            "click .savePic":"savePic",
+            "click img":"showImg"
         },
+        initialize:function(){
+
+        },
+        render:function(){
+            this.$el.html(temp.dailyPicture.pictureItem({item:this.model.toJSON(),width:this.$el.width()}));
+        },
+        savePic:function(e){
+            var id=$(e.target).attr('data-id');
+            var that=this;
+            var qiniuModel=new QiniuModel();
+            qiniuModel.save({"id":id}).then(function (res) {
+                that.$(".savePic").removeClass("savePic").removeClass("btn-primary").html("已收藏");
+                layer.msg("收藏成功,后台正在处理!");
+            });
+        },
+        showImg:function(){
+            var start=thumbnailData.findWhere({id:this.model.get("id")});
+            layer.photos({
+                photos:{
+                    "title":this.model.get("title"),
+                    "id":1,
+                    "start":thumbnailData.indexOf(start),
+                    "data":thumbnailData.toJSON()
+                },
+                closeBtn:true,
+                shift:1
+            });
+        }
+    });
+    var DailyPicture=dc.BaseView.extend({
         initialize:function(){
            this.pagingView=new  dc.PaginatedView({collection:pList});
             pList.bind("reset",this.renderList,this);
@@ -31,24 +77,26 @@ define(['backbone','template','layer','dataCenter'], function(Backbone,temp,laye
         },
         open:function(baseEl,option){
             this.setElement(baseEl);
-            dc.debug("sub view welcome open");
+            dc.debug("sub view daily picture open");
             this.render();
         },
         renderList:function(){
             this.$("#pictureList").empty();
             var that=this;
             pList.each(function(picture){
-                that.$("#pictureList").append(temp.dailyPicture.pictureItem({item:picture.toJSON()}))
+                var thumbnail=new ThumbnailView({model:picture});
+                that.$("#pictureList").append(thumbnail.el);
+                thumbnail.render();
+                //把图片都放到列表里,方便图册查看
+                thumbnailData.add(new Backbone.Model({
+                    "id":picture.get("id"),
+                    "alt":picture.get("alt"),
+                    "pid":picture.get("id"),
+                    "src":picture.getImgSrc(),
+                    "thumb":picture.getImgThumbnailSrc(thumbnail.$el.width())
+                }));
             });
 
-        },
-        savePic:function(e){
-            var id=$(e.target).attr('data-id');
-            var that=this;
-            var qiniuModel=new QiniuModel();
-            qiniuModel.save({"id":id},function(res){
-                layer.msg("收藏成功,后台正在处理!");
-            });
         }
     });
 
